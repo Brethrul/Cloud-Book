063508683c723e24bbc00be77996c1bbbf34d33b
admin <tyc1234@bupt.edu.cn>
2023-08-03 16:51:47 +0800

    hello world

--- src/main/java/com/example/demo/servlet/HelloWorldController.java
+++ src/main/java/com/example/demo/servlet/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.servlet;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
