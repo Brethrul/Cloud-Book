9fab8cd91e99ef0e66500b3618fa7cd8b3415ab7
Brethrul <Tyc1234@bupt.edu.cn>
2023-08-18 22:25:27 +0800

    onclose修复

--- src/main/java/com/example/demo/controller/DocumentEditContriller.java
+++ src/main/java/com/example/demo/controller/DocumentEditContriller.java
@@ -64,15 +64,19 @@ public class DocumentEditContriller {
     /**
      * 连接关闭调用的方法
      */
-//    @OnClose
-//    public void OnClose(){
-//        if(webSocketSet.get(this.filename).contains(this.session))
-//            webSocketSet.get(this.filename).remove(this.session);
-//        if(webSocketSet.get(this.filename).size() == 0)
-//            webSocketSet.remove(this.filename);
-//
-//
-//    }
+    @OnClose
+    public void OnClose() throws IOException {
+        if(webSocketSet.get(this.filename).contains(this)) {
+
+            webSocketSet.get(this.filename).remove(this);
+//            this.session.getBasicRemote().sendText("用户退出，现在还剩"+webSocketSet.get(this.filename).size()+"位用户");
+//            this.session.close();
+        }
+        if(webSocketSet.get(this.filename).size() == 0) {
+            webSocketSet.remove(this.filename);
+        }
+
+    }
 
     /**
      * 收到客户端消息后调用的方法
